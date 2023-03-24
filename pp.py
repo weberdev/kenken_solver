4a9248c5400f844a4ac527fd5157e65b24798b1e
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2023-03-23 23:48:56 -0700

    Simplification of repetitive code

--- pp.py
+++ pp.py
@@ -2,7 +2,7 @@
 from subprocess import run, PIPE
 from functools import reduce
 from itertools import takewhile, dropwhile
-from typing import Union, TypeVar
+from typing import Union
 import json
 import sys
 
@@ -10,24 +10,9 @@ import sys
 COLOR_ON = "\033[95m"
 COLOR_OFF = "\033[0m"
 
-# Ensure we got a puzzle passed to us
-if len(sys.argv) != 2:
-    print("ERROR: expected a single command argument containing a Puzzle ID")
-    print("For example")
-    print("pp '22597'")
-
-puzzle = sys.argv[1]
-
-# Run the provided script from the professor
-p = run(
-    ["bash", "./fetch.sh", puzzle],
-    stdout=PIPE,
-    encoding="ascii",
-)
-if p.returncode != 0:
-    print("Error script did not execute properly")
-    exit(1)
-info = json.loads(p.stdout.replace("\\r\\n", "\\n").replace("\\r", "\\n"))["data"]
+# Constents just used for convinience when printing the grid
+BOLDBAR = COLOR_ON + "|"
+BOLDEQ = COLOR_ON + "="
 
 
 def collectSection(puzzleInfo, startChar: str, endChar: Union[None, str] = None):
@@ -66,143 +51,148 @@ class Cell:
     def __repr__(self) -> str:
         return str(self.vertical)
 
-    def targetStr(self) -> str:
+    def constraint(self) -> str:
         return f"{self.target}{self.operator if (self.operator != '1') else ' '}"
 
 
-target = collectSection(info, "T", "S")
-symbols = collectSection(info, "S", "V")
-answer = collectSection(info, "A", "T")
-vertical = collectSection(info, "V", "H")
-
-# had to transpose
-horazontal = transpose(
-    map(
-        lambda line: [cell for cell in line.split()],
-        [line for line in collectSection(info, "H").split("\n")],
-    )
-)
-
-targetRows = list(
-    map(
-        lambda a, b, c, d, e: list(map(Cell, a, b, c, d, e)),
-        [(line.strip().split()) for line in target.split("\n")],
-        [(line.strip().split()) for line in symbols.split("\n")],
-        [(line.strip().split()) for line in answer.split("\n")],
-        [(line.strip().split() + ["1"]) for line in vertical.split("\n")],
-        [line for line in horazontal + [horazontal[-1]]],
-    ),
-)
-
-largestConstraint = reduce(
-    lambda a, b: max(
-        a,
-        len((b.target + b.operator) if b.target and b.operator else " "),
-    ),
-    [i for l in targetRows for i in l],
-    0,
-)
-
-
-columnWidth = largestConstraint + 2
-
-
-def whiteSpaceFor(string: str) -> str:
-    return " " * (columnWidth - len(string))
-
-
-BOLDBAR = COLOR_ON + "|"
-BOLDEQ = COLOR_ON + "="
-
-
-def topOrBottomLine(colwid):
+def topOrBottomLine(colwid, row):
     # Start of grid
     print(
         COLOR_ON
-        + "|"
-        + "+".join(["=" * columnWidth for _ in (enumerate(targetRows[0]))])
-        + "|"
+        + "+"
+        + "=".join(["=" * colwid for _ in (enumerate(row))])
+        + "+"
         + COLOR_OFF
     )
 
 
-topOrBottomLine(columnWidth)
-for i, row in enumerate(targetRows):
-    # Row with the target and symbol printed
-    if i >= 1:  # We have a dedicated print for just the first grid line alreadyg
-        print(
-            BOLDBAR
-            + "+".join(
-                [
-                    COLOR_OFF
-                    + (
+def createKenKen(puzzleString):
+    target = collectSection(puzzleString, "T", "S")
+    symbols = collectSection(puzzleString, "S", "V")
+    answer = collectSection(puzzleString, "A", "T")
+    vertical = collectSection(puzzleString, "V", "H")
+
+    # had to transpose
+    horazontal = transpose(
+        map(
+            lambda line: [cell for cell in line.split()],
+            [line for line in collectSection(puzzleString, "H").split("\n")],
+        )
+    )
+
+    return list(
+        map(
+            lambda a, b, c, d, e: list(map(Cell, a, b, c, d, e)),
+            [(line.strip().split()) for line in target.split("\n")],
+            [(line.strip().split()) for line in symbols.split("\n")],
+            [(line.strip().split()) for line in answer.split("\n")],
+            [(line.strip().split() + ["1"]) for line in vertical.split("\n")],
+            [line for line in horazontal + [horazontal[-1]]],
+        ),
+    )
+
+
+def main():
+    # Ensure we got a puzzle passed to us
+    if len(sys.argv) != 2:
+        print("ERROR: expected a single command argument containing a Puzzle ID")
+        print("For example")
+        print("pp '22597'")
+
+    puzzle = sys.argv[1]
+
+    # Run the provided script from the professor
+    p = run(
+        ["bash", "./fetch.sh", puzzle],
+        stdout=PIPE,
+        encoding="ascii",
+    )
+    if p.returncode != 0:
+        print("Error script did not execute properly")
+        exit(1)
+    info = json.loads(p.stdout.replace("\\r\\n", "\\n").replace("\\r", "\\n"))["data"]
+
+    kenkenPuzzle = createKenKen(info)
+
+    largestConstraint = reduce(
+        lambda a, b: max(
+            a,
+            len(b.constraint() if b.target and b.operator else " "),
+        ),
+        [i for l in kenkenPuzzle for i in l],
+        0,
+    )
+
+    # How wide a column should be. I added 2 extra space to make it look pretty
+    columnPadding = 2
+    columnWidth = largestConstraint + columnPadding
+
+    # We now begin the grid
+    topOrBottomLine(columnWidth, kenkenPuzzle[0])
+    for rowNumber, row in enumerate(kenkenPuzzle):
+        whiteSpaceFor = lambda s: " " * (columnWidth - len(s))
+        # Row with the target and symbol printed
+        if (
+            rowNumber >= 1
+        ):  # We have a dedicated print for just the first grid line alreadyg
+            print(
+                BOLDBAR
+                + "+".join(
+                    [
                         (
-                            (
-                                BOLDEQ
-                                if (
-                                    c.target
-                                    # If the previous line was a horizontal row
-                                    # then we also need to mark that
-                                    or (
-                                        targetRows[i - 1][j].horazontal
-                                        if i >= 1
-                                        else False
-                                    )
-                                )
-                                else COLOR_OFF + "-"
-                            )
-                            * columnWidth
-                        )
-                        + (
-                            COLOR_ON
+                            COLOR_OFF + (BOLDEQ * columnWidth)
                             if (
                                 c.target
                                 # If the previous line was a horizontal row
                                 # then we also need to mark that
                                 or (
-                                    targetRows[i - 1][j].horazontal if i >= 1 else False
+                                    rowNumber >= 1
+                                    and kenkenPuzzle[rowNumber - 1][j].horazontal
                                 )
-                                or c.vertical
                             )
-                            else ""
+                            else COLOR_OFF + ("-" * columnWidth)
                         )
+                        + (COLOR_ON if c.vertical else "")
+                        for j, c in (enumerate(row))
+                    ]
+                )
+                + BOLDBAR
+            )
+        # Row with the answer printed
+        print(
+            BOLDBAR
+            + "|".join(
+                [
+                    COLOR_OFF
+                    + (
+                        cell.constraint() + whiteSpaceFor(cell.constraint())
+                        if cell.target
+                        else " " * columnWidth
                     )
-                    for j, c in (enumerate(row))
+                    + (COLOR_ON if cell.vertical else "")
+                    for cell in row
                 ]
             )
             + BOLDBAR
         )
-    # Row with the answer printed
-    print(
-        BOLDBAR
-        + "|".join(
-            [
-                COLOR_OFF
-                + (
-                    cell.targetStr() + whiteSpaceFor(cell.targetStr())
-                    if cell.target
-                    else " " * columnWidth
-                )
-                + (COLOR_ON if cell.vertical else "")
-                for cell in row
-            ]
+        # End of row in grid
+        print(
+            BOLDBAR
+            + "|".join(
+                [
+                    COLOR_OFF
+                    + (whiteSpaceFor(cell.answer))
+                    + cell.answer
+                    + (COLOR_ON if cell.vertical else COLOR_OFF)
+                    for cell in row
+                ]
+            )
+            + BOLDBAR
         )
-        + BOLDBAR
-    )
-    # End of row in grid
-    print(
-        BOLDBAR
-        + "|".join(
-            [
-                COLOR_OFF
-                + (whiteSpaceFor(cell.answer))
-                + cell.answer
-                + (COLOR_ON if cell.vertical else COLOR_OFF)
-                for cell in row
-            ]
-        )
-        + BOLDBAR
-    )
 
-# Finally the ending row of =, +, and | all colored since it's the outer edge
-topOrBottomLine(columnWidth)
+    # Finally the ending row of =, +, and | all colored since it's the outer edge
+    topOrBottomLine(columnWidth, kenkenPuzzle[0])
+
+
+if __name__ == "__main__":
+    main()
